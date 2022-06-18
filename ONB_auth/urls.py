2c1ae914c51e6312a1b463123098fbba0c2e77da
the-impossible <richardemmanuel45@gmail.com>
2022-06-18 14:35:40 +0100

    Customized the admin dashboard

--- ONB_auth/urls.py
+++ ONB_auth/urls.py
@@ -5,11 +5,13 @@ from django.urls import path
 from ONB_auth.views import (
     RegisterView,
     LoginView,
+    DashboardView,
 )
 
 app_name = 'auth'
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login')
+    path('login', LoginView.as_view(), name='login'),
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
 ]
--- ONB_auth/views.py
+++ ONB_auth/views.py
@@ -32,3 +32,7 @@ class RegisterView(View):
 class LoginView(View):
     def get(self, request):
         return render(request, 'auth/login.html')
+
+class DashboardView(View):
+    def get(self, request):
+        return render(request, 'auth/dashboard.html')
